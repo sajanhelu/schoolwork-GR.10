8cd62d0d47a61dacee9dc0a934a8f6560027b883
sajanhelu <3722@ajyalfalah.sch.ae>
2024-11-15 14:14:30 +0400

    Add files via upload
    my week 12 ict work

---
+++ school wk 12.py
@@ -0,0 +1,8 @@
+studentname="khalifa"
+school="Ajyal international school"
+age=15
+print("my name is", studentname)
+print("i am studying at:", school)
+print("i am:", age, "years old")
+print( 90 / 22)
+print(786.9 + 76.88 )
---
+++ variables wk 12.py
@@ -0,0 +1,6 @@
+print("welcome khalifa")
+a="87"
+b="980"
+c="85260"
+d="558"
+print("the sum of multiplying", a * b, "is", c, "and times that by", d, "and you have", c * d,)
